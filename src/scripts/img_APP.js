ac2d83257e1c54c1f89df87f38d4eecb05c33a15
akuma-code <akumapl86@gmail.com>
2021-06-12 10:43:50 +0300

    top/right

--- src/scripts/img_APP.js
+++ src/scripts/img_APP.js
@@ -4,7 +4,7 @@ $img_cont.addEventListener('pointerenter', function z_up(event) {
     let target = event.target;
     if (!target.dataset.side) return
         // console.log(`${target.dataset.side}`);
-    if (target.dataset.side) {
+    if (target.dataset.side || target.tagName === 'p') {
         target.style.setProperty('z-index', 100);
         target.setAttribute('current', true);
     }
@@ -14,8 +14,18 @@ $img_cont.addEventListener('pointerout', function z_down(event) {
     let target = event.target;
     if (!target.dataset.side) return
         // console.log(`${target.dataset.side}`);
-    if (target.dataset.side) {
+    if (target.dataset.side || target.tagName === 'p') {
         target.removeAttribute('style')
         target.removeAttribute('current')
     }
 }, true);
+
+const $img_conteiner = document.querySelector('.img_cont');
+
+$img_conteiner.addEventListener('pointerenter', function(event) {
+    let target = event.target;
+    // if (target.tagName !== 'p' || 'div') return;
+    let $out = document.querySelector('div.tps_output');
+    $out.innerHTML = `${target.tagName}>>
+${target.textContent}`;
+}, true)
