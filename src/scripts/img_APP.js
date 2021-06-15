e9ae24d6f4868391b1a9a0804b16b780982856e2
akuma-code <akumapl86@gmail.com>
2021-06-15 22:25:05 +0300

    fix

--- src/scripts/img_APP.js
+++ src/scripts/img_APP.js
@@ -1,31 +1,36 @@
 const $img_cont = document.querySelector('div.img_cont');
+//!----------<<<-----------повесить обработчик на дивы для создания списка выбора------------->>>-------------//
 
-$img_cont.addEventListener('pointerenter', function z_up(event) {
-    let target = event.target;
-    if (!target.dataset.side) return
-        // console.log(`${target.dataset.side}`);
-    if (target.dataset.side || target.tagName === 'p') {
-        target.style.setProperty('z-index', 100);
-        target.setAttribute('current', true);
-    }
-}, true);
+// $img_cont.addEventListener('pointerenter', function z_up(event) {
+//     let target = event.target;
+//     if (!target.dataset.side) return
+//         // console.log(`${target.dataset.side}`);
+//     if (target.dataset.side || target.tagName === 'p') {
+//         target.style.setProperty('z-index', 100);
+//         target.setAttribute('current', true);
+//     }
+// }, true);
+// 
+// $img_cont.addEventListener('pointerout', function z_down(event) {
+//     let target = event.target;
+//     if (!target.dataset.side) return
+//         // console.log(`${target.dataset.side}`);
+//     if (target.dataset.side || target.tagName === 'p') {
+//         target.removeAttribute('style')
+//         target.removeAttribute('current')
+//     }
+// }, true);
 
-$img_cont.addEventListener('pointerout', function z_down(event) {
-    let target = event.target;
-    if (!target.dataset.side) return
-        // console.log(`${target.dataset.side}`);
-    if (target.dataset.side || target.tagName === 'p') {
-        target.removeAttribute('style')
-        target.removeAttribute('current')
-    }
-}, true);
-
-const $img_conteiner = document.querySelector('.img_cont');
+const $img_conteiner = document.querySelector('.tps_img');
 
 $img_conteiner.addEventListener('pointerenter', function(event) {
     let target = event.target;
     // if (target.tagName !== 'p' || 'div') return;
     let $out = document.querySelector('div.tps_output');
-    $out.innerHTML = `${target.tagName}>>
-${target.textContent}`;
+    $out.innerHTML = '';
+    $out.insertAdjacentText('afterbegin', `<${target.tagName}> ${target.textContent || 'none('}`)
+
 }, true)
+
+//TODO: добавить обработчик на сайды, чтобы отображалось выбранное по клику
+//
