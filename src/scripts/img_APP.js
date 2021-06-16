6b1e0f2d78593030e86e4308166b9c009b3ccc12
akuma-code <akumapl86@gmail.com>
2021-06-16 20:34:30 +0300

    add system and sides

--- src/scripts/img_APP.js
+++ src/scripts/img_APP.js
@@ -1,6 +1,43 @@
+const $img_conteiner = document.querySelector('.tps_img');
 const $img_cont = document.querySelector('div.img_cont');
+const $sys = document.querySelector('div.tps_sys>span');
+const $syscontent = document.querySelector('.tps_sys_content');
 //!----------<<<-----------повесить обработчик на дивы для создания списка выбора------------->>>-------------//
 
+
+
+$img_conteiner.addEventListener('pointerenter', function(event) {
+    let target = event.target;
+    // if (target.tagName !== 'p' || 'div') return;
+    let $out = document.querySelector('div.tps_output');
+    $out.innerHTML = '';
+    $out.insertAdjacentText('afterbegin', `<${target.tagName}> ${target.textContent || 'none('}`)
+
+}, true)
+
+$syscontent.addEventListener('click', function(event) {
+    let target = event.target;
+    $sys.innerText = '';
+    for (let item of $syscontent.children) {
+        item.classList.remove('selected')
+    }
+    target.classList.add('selected')
+    $sys.insertAdjacentHTML('afterbegin', target.textContent)
+        // if (target.classList.has('tps_sys_content')) {    }
+}, true)
+
+
+
+
+
+//TODO: добавить обработчик на сайды, чтобы отображалось выбранное по клику
+
+
+
+
+
+
+//
 // $img_cont.addEventListener('pointerenter', function z_up(event) {
 //     let target = event.target;
 //     if (!target.dataset.side) return
@@ -20,17 +57,3 @@ const $img_cont = document.querySelector('div.img_cont');
 //         target.removeAttribute('current')
 //     }
 // }, true);
-
-const $img_conteiner = document.querySelector('.tps_img');
-
-$img_conteiner.addEventListener('pointerenter', function(event) {
-    let target = event.target;
-    // if (target.tagName !== 'p' || 'div') return;
-    let $out = document.querySelector('div.tps_output');
-    $out.innerHTML = '';
-    $out.insertAdjacentText('afterbegin', `<${target.tagName}> ${target.textContent || 'none('}`)
-
-}, true)
-
-//TODO: добавить обработчик на сайды, чтобы отображалось выбранное по клику
-//
