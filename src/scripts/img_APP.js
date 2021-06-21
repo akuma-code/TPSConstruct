651b414a5b4962437afb6069f0d03a009b108103
akuma-code <akumapl86@gmail.com>
2021-06-21 22:14:08 +0300

    remake data*

--- src/scripts/img_APP.js
+++ src/scripts/img_APP.js
@@ -12,64 +12,55 @@ const $out = document.querySelector('div.tps_output');
 const $sides = document.querySelectorAll('.img_side');
 const $main = document.querySelector('div.tps_main');
 const $outputelem = {
-    system: '[output=system]',
-    leftside: '[output=leftside]',
-    topside: '[output=topside]',
-    rightside: '[output=rightside]',
-    botside: '[output=botside]',
-    out: '[output=out]',
-
-}
-
+        system: '[output=system]',
+        left: '[output=left]',
+        top: '[output=top]',
+        right: '[output=right]',
+        bot: '[output=bot]',
+        out: '[output=out]',
+    }
+    //! Обработчик на контейнер изображения
 $img_cont.addEventListener('click', function(event) {
-    let target = event.target;
+        let target = event.target;
 
-    if (target.classList.contains('img_cont')) {
-        $out.innerHTML = ''
-        return
-    };
-    $out.innerHTML = '';
-    $out.insertAdjacentText('afterbegin', `${event.target.dataset.side || 'Selected'}: ${target.textContent || 'none('}`);
+        // if (target.classList.contains('img_cont')) {
+        //     // $out.innerHTML = ''
+        //     return
+        // };
+        $out.innerHTML = '';
+        $out.insertAdjacentText('afterbegin', `${event.target.dataset.side || 'Selected'}: ${target.textContent || 'none('}`);
+        // debugger
+        $out.innerHTML = setOutput(target.tagName, target.textContent)
 
-}, true)
-
+    }, true)
+    //! ОБЩИЙ ОБРАБОТЧИК
 $main.addEventListener('click', function(e) {
     let target = e.target;
     // if (target.matches('ul.side_content>li') || target.matches('ul.tps_sys_content>li')) {
 
     if (target.matches('li')) {
 
-        document.querySelector(`${$outputelem[target.dataset.outtype]}`).innerText = target.textContent
-
-
-
+        document.querySelector(`${$outputelem[target.closest('ul').dataset.side]}`).innerText = target.textContent
 
         for (let elem of target.closest('ul').children) {
-            // elem.classList.remove('selected')
-            elem.removeAttribute('class')
+            elem.classList.remove('selected')
         }
 
         target.classList.add('selected');
-
     }
-
-    return
-
-
 }, true)
 
-// $sys_content.addEventListener('click', function(event) {
-//     let target = event.target;
-//     $out.innerHTML = target.tagName;
-//     $sys.innerText = '';
-//     for (let item of $sys_content.children) {
-//         item.classList.remove('selected')
-//             // item.style.setProperty('font-size', '1rem')
-//             // item.style.setProperty('background-color', 'aqua')
-//     }
-//     target.classList.add('selected');
-//     // target.style.setProperty('font-size', '1.3rem')
-//     // target.style.setProperty('background-color', 'brown')
-//     $sys.insertAdjacentHTML('afterbegin', target.textContent)
-//         // if (target.classList.has('tps_sys_content')) {    }
-// }, true)
+function setOutput(target, text) {
+    const $ul = `<ul>
+<li>
+    <span>EventTarget: ${target}</span>
+</li>
+<li>
+    <span>Text: ${text}</span>
+</li>
+
+</ul>
+`;
+
+    return $ul
+}
