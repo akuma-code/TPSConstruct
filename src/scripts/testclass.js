2eb390b1be6b3229ab4b53357bfa42def92e82c5
akuma-code <akumapl86@gmail.com>
2021-08-10 14:36:44 +0300

    html_output

--- src/scripts/testclass.js
+++ src/scripts/testclass.js
@@ -65,68 +65,78 @@ class Manipulator {
 
 }
 
+const modelOutput = {
+    svet: [{
+            id: '#out_sizes',
+            label: 'Исходные размеры:',
+            html_out: 'html_sizes',
+        },
+        {
+            id: '#ms_simple',
+            label: 'М/С:',
+            html_out: 'html_simple',
+        },
+        {
+            id: '#ms_skf',
+            label: 'М/С SKF:',
+            html_out: 'html_skf',
+        },
+    ],
+    fix: [{
+            id: '#out_sizes',
+            label: 'Исходные размеры:',
+            html_out: 'html_sizes',
+        },
+        {
+            id: '#out_glass',
+            label: 'Стеклопакет:',
+            html_out: 'html_glass',
+        },
+        {
+            id: '#out_stv',
+            label: 'Створка:',
+            html_out: 'html_stv',
+        },
+    ],
+    stv: [{
+            id: '#out_sizes',
+            label: 'Исходные размеры:',
+            html_out: 'html_sizes',
+        },
+        {
+            id: '#out_glass',
+            label: 'Стеклопакет:',
+            html_out: 'html_glass',
+        },
+        {
+            id: '#out_stv',
+            label: 'Створка:',
+            html_out: 'html_stv',
+        },
+        {
+            id: '#ms_simple',
+            label: 'М/С:',
+            html_out: 'html_simple',
+        },
+        {
+            id: '#ms_skf',
+            label: 'М/С SKF:',
+            html_out: 'html_skf',
+        },
+    ],
+};
+
 class TPS_HTML_Output {
     constructor() {
+        this.templateOutput = modelOutput
+    }
 
+    getModel(type) {
+        const result = modelOutput[type]
+        return result
     };
 
-    model(state) {
-
-        const outputmodel = {
-            svet: [{
-                    id: '#out_sizes',
-                    label: 'Исходные размеры:',
-                },
-                {
-                    id: '#ms_simple',
-                    label: 'М/С:',
-                },
-                {
-                    id: '#ms_skf',
-                    label: 'М/С SKF:',
-                },
-            ],
-            fix: [{
-                    id: '#out_sizes',
-                    label: 'Исходные размеры:',
-                },
-                {
-                    id: '#out_glass',
-                    label: 'Стеклопакет:',
-                },
-                {
-                    id: '#out_stv',
-                    label: 'Створка:',
-                },
-            ],
-            stv: [{
-                    id: '#out_sizes',
-                    label: 'Исходные размеры:',
-                },
-                {
-                    id: '#out_glass',
-                    label: 'Стеклопакет:',
-                },
-                {
-                    id: '#out_stv',
-                    label: 'Створка:',
-                },
-                {
-                    id: '#ms_simple',
-                    label: 'М/С:',
-                },
-                {
-                    id: '#ms_skf',
-                    label: 'М/С SKF:',
-                },
-            ],
-        };
-
-        return outputmodel[state]
-    };
-
-    getSizes(sizeObject = {}) {
-        let currentmodel = this.model(sizeObject.type);
+    getSizes(sizeObject) {
         currentmodel.map(element => {
             if (element.id === sizeObject.id) {
 
@@ -138,19 +148,48 @@ class TPS_HTML_Output {
         return currentmodel
     };
 
-    labelToHTML(HTMLObject) {
-        function wraplabel(label) {
-            return `<span>${label}</span>`
-        };
+    getHTMLObject(type) {
+        let Text2Html = [];
+        let MODEL = this.getModel(type);
+        console.dir({ model: MODEL });
+        MODEL.forEach(element => {
+            Text2Html.push(`<div data-output=${element.html_out}>${element.label}</div>`)
+        });
+        // console.table(resultHTML);
+        return Text2Html
+    };
 
-        function wrapline(id = '', cls = '') {
-            return `<div class=${cls} id=${id}>${wraplabel(label)}`
-        };
+};
 
-        let HtmlObject = this.model
-    }
+function renderHTML(HtmlTextObjects = []) {
+    const output = document.querySelector('[data-output=html_out]');
+    output.innerHTML = ''
+    HtmlTextObjects.forEach(TextElement => {
+        output.insertAdjacentHTML('beforeend', TextElement)
+    })
 }
 
+function TPS_addHandler() {
+    const $tgl_box = document.querySelector('div.tgl_big_box');
+    const buttons = document.getElementsByClassName('ts');
+    for (button of buttons) {
+
+        button.addEventListener('click', function(event) {
+            let t = event.target;
+            let type = t.dataset.type_sel;
+            const state = document.querySelector('[data-html-type]');
+            state.dataset.htmlType = type;
+            let HTML_OUT = new TPS_HTML_Output()
+            renderHTML(HTML_OUT.getHTMLObject(type))
+
+            return console.log(HTML_OUT)
+        })
+
+    }
+
+}
+
+TPS_addHandler();
 
 let test_sizeobj = {
     w: 1234,
@@ -158,4 +197,3 @@ let test_sizeobj = {
     id: '#out_stv',
     type: 'stv'
 }
-let testhtml_stv = new TPS_HTML_Output().getSizes(test_sizeobj);
